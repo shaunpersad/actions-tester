e35d48a8830bc0796604bdcb6935c1dcfed9cd56
spersad <spersad@cloudflare.com>
2025-05-06 13:55:14 -0400

    save more data

--- number.js
+++ number.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const prId = Number(fs.readFileSync('./_pr/number'));
-const repository = fs.readFileSync('./_pr/repository');
-const sha = fs.readFileSync('./_pr/sha');
+const repository = fs.readFileSync('./_pr/repository', 'utf8');
+const sha = fs.readFileSync('./_pr/sha', 'utf8');
 
 console.log({ prId, repository, sha })
